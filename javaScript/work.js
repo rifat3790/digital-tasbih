7350fb3a728aa5af46ded28b9ace3135482a7843
rifat3790 <mdrifayethossen@gmail.com>
2023-08-13 19:53:11 +0600

    create digital tasbih using javascript

---
+++ javaScript/work.js
@@ -0,0 +1,20 @@
+// Subhan Allah 
+const item1Display = document.getElementById('item-1-display');
+const item1Increment = document.getElementById('item-1-increment');
+const item1decrement = document.getElementById('item-1-decrement');
+
+// Alhamdulillah
+const item2Display = document.getElementById('item-2-display');
+const item2Increment = document.getElementById('item-2-increment');
+const item2decrement = document.getElementById('item-2-decrement');
+
+// Allahuakber
+const item3Display = document.getElementById('item-3-display');
+const item3Increment = document.getElementById('item-3-increment');
+const item3decrement = document.getElementById('item-3-decrement');
+
+// Reset button
+const reset = document.getElementById('reset-btn');
+
+
+// initial value set
